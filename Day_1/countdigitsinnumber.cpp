9a663edd3a8efdcba783f64dee85a3be5079a036
Vivek Yadav <ervivek6393@gmail.com>
2026-05-27 18:15:11 +0530

    Add files via upload

---
+++ Day_1/countdigitsinnumber.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    int count = 0;
+    cout<<"Enter a number: ";
+    cin>>n;
+    while(n > 0){
+        n = n/10;
+        count++;
+    }
+    cout<<"Digits in a given number is: "<<count<<endl;
+    return 0;
+
+}
